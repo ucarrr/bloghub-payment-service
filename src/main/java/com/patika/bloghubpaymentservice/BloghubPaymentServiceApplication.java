1e8ff6e89d0669482d5b2ba9c47a091e6f0c8faa
ucarrr <ucaaar07@gmail.com>
2024-08-12 00:43:18 +0300

    initial commit

---
+++ src/main/java/com/patika/bloghubpaymentservice/BloghubPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.patika.bloghubpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloghubPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloghubPaymentServiceApplication.class, args);
+    }
+
+}
